f91ec918103ff74a9cba5b0247953c4d70866b00
Tony Hirst <tony.hirst@gmail.com>
2019-01-22 11:36:36 +0000

    Dakar rough and ready SQL table defs

---
+++ doodles/dakar.sql
@@ -0,0 +1,90 @@
+CREATE TABLE "teams" (
+    "Year" INTEGER,
+    "Bib" INTEGER,
+    "Team" TEXT,
+    PRIMARY KEY ("Year","Bib")
+);
+
+
+CREATE TABLE "crew" (
+    "Year" INTEGER,
+    "Bib" INTEGER,
+    "Num" INTEGER,
+    "Name" TEXT,
+    "Country" TEXT,
+    PRIMARY KEY ("Year","Bib","Num")
+);
+
+CREATE TABLE "vehicles" (
+    "Year" INTEGER,
+    "Bib" INTEGER,
+    "VehicleType" TEXT,
+    "Brand" TEXT,
+    PRIMARY KEY ("Year","Bib")
+);
+
+
+
+CREATE TABLE "stagestats" (
+    "Year" INTEGER,
+    "Stage" INTEGER,
+    "Start" TEXT,
+    "Liaison" TEXT,
+    "Special" TEXT,
+    "AtStart" INTEGER,
+    "Left" INTEGER,
+    "Arrived" INTEGER,
+    "LatestWP" TEXT,
+    "LeaderLatestWP" TEXT,
+    "NumLatestWP" TEXT,
+    "BibLatestWP" INTEGER,
+    "NameLatestWP" TEXT,
+    "Vehicle" TEXT,
+    PRIMARY KEY ("Year","Stage")
+);
+
+
+CREATE TABLE "ranking" (
+    "Year" INTEGER,
+    "Stage" INTEGER,
+    "Type" TEXT, --timerank
+    "Pos" INTEGER,
+    "Bib" INTEGER,
+    "VehicleType" TEXT,
+    "Crew" TEXT,
+    "Brand" TEXT,
+    "Time_raw" TEXT,
+    "TimeInS" INTEGER,
+    "Gap_raw" TEXT,
+    "GapInS" INTEGER,
+    "Penalty_raw" TEXT,
+    "PenaltyInS" INTEGER,
+    PRIMARY KEY ("Year","Stage", "Type", "Bib")
+);
+
+CREATE TABLE "stagemeta" (
+    "Year" INTEGER,
+    "Stage" INTEGER,
+    "Bib" INTEGER,
+    "RoadPos" REAL,
+    "Refuel" TEXT,
+    PRIMARY KEY ("Year","Stage","Bib")
+);
+
+CREATE TABLE "waypoints" (
+    "Year" INTEGER,
+    "Stage" INTEGER,
+    "Bib" INTEGER,
+    "Pos" INTEGER,
+    "Waypoint" TEXT,
+    "WaypointOrder" INTEGER,
+    "WaypointRank" INTEGER,
+    "Time_raw" TEXT,
+    "TimeInS" INTEGER,
+    "Gap_raw" TEXT,
+    "GapInS" INTEGER,
+    PRIMARY KEY ("Year", "Stage", "Bib", "Waypoint")
+);
+
+--there is also a fuller waypoints dataset with kind='full' results, eg ito pos at each split
+
